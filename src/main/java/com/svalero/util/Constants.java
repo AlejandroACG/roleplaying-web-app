decb95febf9c4e6c4250569d04f8860c0c1f641d
Alejandro A. Cristóbal <acristobalgarces@hotmail.com>
2023-05-12 13:16:28 +0200

    Fixes #2: space in database username erased

--- src/main/java/com/svalero/util/Constants.java
+++ src/main/java/com/svalero/util/Constants.java
@@ -2,6 +2,6 @@ package com.svalero.util;
 
 public class Constants {
     public static String DATABASE_URL = "jdbc:oracle:thin:@localhost:1521/xe";
-    public static String DATABASE_USERNAME = " Alumno";
+    public static String DATABASE_USERNAME = "Alumno";
     public static String DATABASE_PASSWORD = "1234";
 }
